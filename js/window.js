53def9874f5ba78a80943e2d196af0cf36d50c07
Yield4us <yield4us@hotmail.com>
2014-03-24 18:16:10 +0600

    Вся работа сделанная 22 марта 2014
    Добавлен пункт Имидж , Контакты

---
+++ js/window.js
@@ -0,0 +1,58 @@
+
+$(function(){
+
+	$('#layer1').click(function(){
+		$('.lay1,.hight-layout').fadeIn(500);
+	});
+
+	$('#layer2').click(function(){
+		$('.lay2,.hight-layout').fadeIn(500);
+	});
+
+	$('#layer3').click(function(){
+		$('.lay3,.hight-layout').fadeIn(500);
+	});
+
+	$('#layer4').click(function(){
+		$('.lay4,.hight-layout').fadeIn(500);
+	});
+
+	$('#layer5').click(function(){
+		$('.lay5,.hight-layout').fadeIn(500);
+	});
+
+	$('#layer6').click(function(){
+		$('.lay6,.hight-layout').fadeIn(500);
+	});
+
+	
+
+	$('.hight-layout').click(function(){
+		$('.lay1,.hight-layout').fadeOut(500);
+	});
+    
+    $('.hight-layout').click(function(){
+		$('.lay2,.hight-layout').fadeOut(500);
+	});
+
+	$('.hight-layout').click(function(){
+		$('.lay3,.hight-layout').fadeOut(500);
+	});
+
+	$('.hight-layout').click(function(){
+		$('.lay4,.hight-layout').fadeOut(500);
+	});
+
+	$('.hight-layout').click(function(){
+		$('.lay5,.hight-layout').fadeOut(500);
+	});
+
+	$('.hight-layout').click(function(){
+		$('.lay6,.hight-layout').fadeOut(500);
+	});
+
+	
+
+
+	
+});
