f07a4421d7bc24e70d8ffec3babc02cde16468fb
a9n <ayan_9999@mail.ru>
2014-03-24 19:02:29 +0600

    Добавлен слайдер

---
+++ js/slider-script.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($){
+	var easing = 'swing';
+	$(#slider).parallaxSlider({
+		autoPlay5000,false to disable, otherwise int duration in ms hold between slides
+		speed2000, transition duration between slides
+		easing 'swing',
+		numBackgrounds 4,
+		thumbRotationfalse,
+		 hash false,
+		 debugtrue,
+		css3 true,
+		easingCSS3 'cubic-bezier(0.000, 0.265, 1.000, 0.005)',
+	});
+})
